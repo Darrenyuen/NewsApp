0c8cd92bf6d3b98680534b2c50b6e95210e8f9ab
ysf <yuansssf@gmail.com>
2019-07-05 17:04:38 +0800

    finish news bean

--- app/src/main/java/com/example/yuan/newsapp/MainActivity.java
+++ app/src/main/java/com/example/yuan/newsapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.yuan.newsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private List<Data> newsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        listView = findViewById(R.id.listView);
+        NewsAdapter newsAdapter = new NewsAdapter(this, R.layout.list_item, newsList);
+        listView.setAdapter(newsAdapter);
     }
 }
--- app/src/main/java/com/example/yuan/newsapp/NewsBean.java
+++ app/src/main/java/com/example/yuan/newsapp/NewsBean.java
@@ -3,12 +3,46 @@ package com.example.yuan.newsapp;
 import java.util.List;
 
 public class NewsBean {
-    String reason;
-    class Result {
+
+    private String reason;
+    private int error_code;
+    private List<Result> results;
+
+    NewsBean(String reason, int error_code, List<Result> results) {
+        this.reason = reason;
+        this.error_code = error_code;
+        this.results = results;
+    }
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+    static class Result {
         String stat;
         List<Data> list;
-        class Data {
-
+        Result(String stat, List<Data> list) {
+            this.stat = stat;
+            this.list = list;
         }
     }
 }
