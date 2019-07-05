77aed297bfb7e1712a01bc2cfb4c3a1b4a879b6d
ysf <yuansssf@gmail.com>
2019-07-05 15:25:20 +0800

    add a data bean

---
+++ app/src/main/java/com/example/yuan/newsapp/Data.java
@@ -0,0 +1,13 @@
+package com.example.yuan.newsapp;
+
+public class Data {
+    private String uniquekey;
+    private String title;
+    private String date;
+    private String category;
+    private String author_name;
+    private String url;
+    private String thumbnail_pic_s;
+    private String thumbnail_pic_s02;
+    private String getThumbnail_pic_s03;
+}
---
+++ app/src/main/java/com/example/yuan/newsapp/NewsBean.java
@@ -0,0 +1,14 @@
+package com.example.yuan.newsapp;
+
+import java.util.List;
+
+public class NewsBean {
+    String reason;
+    class Result {
+        String stat;
+        List<Data> list;
+        class Data {
+
+        }
+    }
+}
