9035c4855aa4de6f78f0dfbab82cf957a8ced7fc
ysf <yuansssf@gmail.com>
2019-07-09 17:06:09 +0800

    add a new activity to show news data

---
+++ app/src/main/java/com/example/yuan/newsapp/ContentActivity.java
@@ -0,0 +1,24 @@
+package com.example.yuan.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ContentActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        String url = getIntent().getStringExtra("url");
+        webView.loadUrl(url);
+    }
+}
