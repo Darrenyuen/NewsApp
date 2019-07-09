b0a7853209b7fddf38e568cd5be2cfc879adfcf5
ysf <yuansssf@gmail.com>
2019-07-09 17:04:39 +0800

    request is successful

--- app/src/main/java/com/example/yuan/newsapp/MainActivity.java
+++ app/src/main/java/com/example/yuan/newsapp/MainActivity.java
@@ -1,21 +1,91 @@
 package com.example.yuan.newsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.yuan.newsapp.Bean.News;
+import com.example.yuan.newsapp.Bean.NewsBean;
+import com.example.yuan.newsapp.util.HttpUtil;
+import com.example.yuan.newsapp.util.ParseUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "MainActivity";
+
     private ListView listView;
-    private List<Data> newsList;
+    private List<NewsTitle> newsTitleList = new ArrayList<>();
+    private NewsTitleAdapter newsTitleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        requestNews();
+
         listView = findViewById(R.id.listView);
-        NewsAdapter newsAdapter = new NewsAdapter(this, R.layout.list_item, newsList);
-        listView.setAdapter(newsAdapter);
+        newsTitleAdapter = new NewsTitleAdapter(this, R.layout.list_item, newsTitleList);
+        listView.setAdapter(newsTitleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            Intent intent = new Intent(MainActivity.this, ContentActivity.class);
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NewsTitle newsTitle = newsTitleList.get(position);
+                intent.putExtra("url", newsTitle.getUrl());
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void requestNews() {
+        String url = "http://v.juhe.cn/toutiao/index?type=top&key=c9bafcd7aff837f20267aa453ff843e4";
+        HttpUtil.sendOkHttpRequest(url, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "加载成功", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                final String responseText = response.body().string();
+                final NewsBean newsBean = ParseUtil.paresJsonWithGson(responseText);
+                final String reason = newsBean.reason;
+                Log.d(TAG, "onResponse: " + reason);
+                if (reason.equals("成功的返回")) {
+                    newsTitleList.clear();
+//                    Toast.makeText(MainActivity.this, "加载成功", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onResponse: " + newsBean.error_code + " " + newsBean.reason);
+                    for (News news : newsBean.result.data) {
+                        NewsTitle newsTitle = new NewsTitle(news.title, news.date, news.category, news.author_name, news.url);
+                        newsTitleList.add(newsTitle);
+                    }
+                }
+            }
+        });
     }
 }
